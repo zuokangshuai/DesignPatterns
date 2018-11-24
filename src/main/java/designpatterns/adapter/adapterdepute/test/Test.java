27957d46c99a38991e6fbaee701ca77945aa17f7
kangshuai.zuo <kangshuai.zuo@luckincoffee.com>
2018-11-24 16:35:09 +0800

    ADD Adapter模式图

--- src/main/java/designpatterns/adapter/adapterdepute/test/Test.java
+++ src/main/java/designpatterns/adapter/adapterdepute/test/Test.java
@@ -2,17 +2,16 @@ package designpatterns.adapter.adapterdepute.test;
 
 
 import designpatterns.adapter.adapterdepute.core.PlugAdapter;
-import designpatterns.adapter.adapterdepute.core.ThreePlug;
 
 /**
- * @author Kangshuai Zuo
  * @description 测试类
+ * @author Kangshuai Zuo
  * @date 2018/11/22 17:49
  */
 public class Test {
 
     public static void main(String[] args) {
-        ThreePlug threePlug = new PlugAdapter("三角插头");
-        ((PlugAdapter) threePlug).threePrint();
+        PlugAdapter threePlug = new PlugAdapter("三角插头");
+        threePlug.threePrint();
     }
 }
